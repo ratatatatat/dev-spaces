5f277b741558f0a64d39489345f918c2c4785112
Rajiv Puri <ratatatatat@users.noreply.github.com>
2024-01-27 18:09:55 -0500

    clean exit

--- src/main/main.ts
+++ src/main/main.ts
@@ -7,12 +7,11 @@ import * as url from 'url';
 import { DBService } from '../Types';
 import { exec } from 'child_process';
 import log from 'electron-log';
-// import('fix-path').then((fixPath: any) => {
-// 	// console.log('fixPath', fixPath.default());
-// 	fixPath.default();
-// })
 
 let mainWindow: Electron.BrowserWindow | null;
+let server: express.Express;
+let initialized = false;
+
 process.on('uncaughtException', (error) => {
 	log.error(error);
 });
@@ -46,56 +45,73 @@ const teardown = () => {
 	serviceTerminalManager.kill();
 }
 
-const createWindow = () => {
-	const server = express();
-	server.use(cors()); // Enable CORS for all domains
-	server.use(express.json());
-	server.use(express.urlencoded({ extended: true }));
-	server.use('/', routes); // Add routes to express server
-	server.listen(3000, () => {
-		mainWindow = new BrowserWindow({
-			width: 1000,
-			minWidth: 1000,
-			height: 600,
-			title: 'Service Commander',
-			icon: path.join(__dirname, "./assets/icons/Template64.png"),
-			webPreferences: {
-				nodeIntegration: true,
-				contextIsolation: true,
-				preload: path.join(__dirname, 'preload.js'), // path to your preload script
-			},
-		});
-		mainWindow.on('closed', () => {
-			mainWindow = null;
-		});
-		serviceTerminalManager.on('data', payload => {
-			mainWindow?.webContents.send('service-terminal-data', payload);
-		})
-		if (process.env.NODE_ENV === 'development') {
-			mainWindow.loadURL('http://localhost:9000');
-			mainWindow.webContents.openDevTools(); // Open the DevTools
-		} else {
-			mainWindow.loadURL(
-				url.format({
-					pathname: path.join(__dirname, '../', 'index.html'),
-					protocol: 'file:',
-					slashes: true,
-				})
-			)
-		}
+
+const initialize = async () => {
+	if (!initialized) {
+		await startServer();
 		registerMainEvents();
+		process.on('SIGINT', () => {
+			app.exit(0);
+		});
+		initialized = true;
+	}
+}
+const startServer = (): Promise<void> => {
+	return new Promise((resolve) => {
+		server = express();
+		server.use(cors()); // Enable CORS for all domains
+		server.use(express.json());
+		server.use(express.urlencoded({ extended: true }));
+		server.use('/', routes); // Add routes to express server
+		server.listen(3000, () => {
+			resolve();
+		});
 	});
-	process.on('SIGINT', () => {
-		app.exit(0);
+};
+
+const createWindow = () => {
+	mainWindow = new BrowserWindow({
+		width: 1000,
+		minWidth: 1000,
+		height: 600,
+		title: 'Service Commander',
+		icon: path.join(__dirname, "./assets/icons/Template64.png"),
+		webPreferences: {
+			nodeIntegration: true,
+			contextIsolation: true,
+			preload: path.join(__dirname, 'preload.js'), // path to your preload script
+		},
 	});
+	mainWindow.on('closed', () => {
+		mainWindow = null;
+	});
+	serviceTerminalManager.on('data', payload => {
+		mainWindow?.webContents.send('service-terminal-data', payload);
+	})
+	if (process.env.NODE_ENV === 'development') {
+		mainWindow.loadURL('http://localhost:9000');
+		mainWindow.webContents.openDevTools(); // Open the DevTools
+	} else {
+		mainWindow.loadURL(
+			url.format({
+				pathname: path.join(__dirname, '../', 'index.html'),
+				protocol: 'file:',
+				slashes: true,
+			})
+		)
+	}
+};
+
+
+const onReady = async () => {
+	await initialize();
+	createWindow();
 }
 
-app.on('ready', createWindow);
+app.on('ready', onReady);
 
 app.on('window-all-closed', () => {
-	if (process.platform !== 'darwin') {
-		app.quit();
-	}
+	app.quit();
 });
 
 app.on('activate', () => {
